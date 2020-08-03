4fb61fc1f0eab4c6f2ac06dd7b95411f59476685
unknown <bahaagamer@mail.ru>
2020-08-03 12:39:19 +0300

    SecondStageOfProjectDeveloping

---
+++ src/main/java/main/model/Post.java
@@ -0,0 +1,112 @@
+package main.model;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name = "posts")
+public class Post {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(nullable = false)
+    private int id;
+
+    @Column(nullable = false, name = "is_active")
+    private byte isActive;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false, name = "moderation_status", columnDefinition = "enum('NEW', 'ACCEPTED', 'DECLINED')")
+    private ModerationStatus moderationStatus;
+
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "moderator_id")
+    private User moderator;
+
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @Column(nullable = false)
+    private Date time;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String text;
+
+    @Column(nullable = false, name = "view_count")
+    private int viewCount;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public byte getIsActive() {
+        return isActive;
+    }
+
+    public void setIsActive(byte isActive) {
+        this.isActive = isActive;
+    }
+
+    public ModerationStatus getModerationStatus() {
+        return moderationStatus;
+    }
+
+    public void setModerationStatus(ModerationStatus moderationStatus) {
+        this.moderationStatus = moderationStatus;
+    }
+
+    public User getModerator() {
+        return moderator;
+    }
+
+    public void setModerator(User moderator) {
+        this.moderator = moderator;
+    }
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Date getTime() {
+        return time;
+    }
+
+    public void setTime(Date time) {
+        this.time = time;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public int getViewCount() {
+        return viewCount;
+    }
+
+    public void setViewCount(int viewCount) {
+        this.viewCount = viewCount;
+    }
+}
